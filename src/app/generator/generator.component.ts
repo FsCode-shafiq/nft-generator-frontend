4ec0f4282bb6e06199777d98bab2882897b6b40e
Shafiq Ur Rehman <69121735+FsCode-shafiq@users.noreply.github.com>
2022-04-24 18:39:35 +0500

    working in generator

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
+import { from, Subject, takeUntil } from 'rxjs';
 import { MessageServiceService } from '../Services/message-service.service';
 import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SnackbarService } from '../Services/snackbar.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-generator',
@@ -13,26 +16,76 @@ export class GeneratorComponent implements OnInit {
   unsubscribe = new Subject();
 
   showInformation = true;
+  showGenartingScreen = false;
   files: NgxFileDropEntry[] = [];
-
+  generatorForm: FormGroup | any;
+  layers!: FormArray;
   constructor(
-    private msg: MessageServiceService
+    private msg: MessageServiceService,
+    private fb: FormBuilder,
+    private snack: SnackbarService,
   ) { }
 
   ngOnInit(): void {
-    this.msg.currentMsg.pipe(takeUntil(this.unsubscribe)).subscribe(msg=> this.msgHandler(msg));
+    
+    this.setAttr();
+    this.msg.currentMsg.pipe(takeUntil(this.unsubscribe)).subscribe(msg => this.msgHandler(msg));
   }
 
-  msgHandler = (msg: any) =>{
+  msgHandler = (msg: any) => {
     switch (msg) {
       case 'proceed':
-        this.showInformation = false
+        this.proceed();
         break;
       case 'back':
         this.showInformation = true;
         break;
+      case 'show-generating-screen':
+        this.showGenartingScreen = true;
+        break;
+
       default:
         break;
+    }
+  }
+
+  setAttr() {
+    this.generatorForm = this.fb.group({
+      collectionName: ['', Validators.required],
+      collectionSize: ['', Validators.required],
+      width: ['', Validators.required],
+      height: ['', Validators.required],
+      discription: ['', Validators.required],
+      layers: new FormArray([])
+    })
+  }
+
+  addLayer() {
+    this.layers = this.generatorForm.get('layers') as FormArray;
+    this.layers.push(
+      this.fb.group({
+        layerName: 'Layer',
+        images: [new FormArray([]), Validators.required]
+      })
+    );
+  }
+
+  deleteLayer(i: any) {
+    const layers: FormArray = this.generatorForm.controls.layers;
+    layers.removeAt(i);
+  }
+
+  proceed() {
+    let isVlaid = true;
+    const formKeys = Object.keys(this.generatorForm.value);
+    formKeys.forEach((key: String) => {
+      if (!this.generatorForm.get(key).value && key !== 'layers') {
+        isVlaid = false;
+        this.snack.openSnackBar('Please Fill all Required Fields')
+      }
+    });
+    if (isVlaid) {
+      this.showInformation = false
     }
   }
 
@@ -44,25 +97,15 @@ export class GeneratorComponent implements OnInit {
       if (droppedFile.fileEntry.isFile) {
         const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
         fileEntry.file((file: File) => {
+          console.log(file);
+          const fileReader: FileReader = new FileReader();
 
-          // Here you can access the real file
-          console.log(droppedFile.relativePath, file);
+          fileReader.onload = () => {
+            // console.log(fileReader.result);
+            
+          }
 
-          /**
-          // You could upload it like this:
-          const formData = new FormData()
-          formData.append('logo', file, relativePath)
-
-          // Headers
-          const headers = new HttpHeaders({
-            'security-token': 'mytoken'
-          })
-
-          this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
-          .subscribe(data => {
-            // Sanitized logo returned from backend
-          })
-          **/
+          fileReader.readAsDataURL(file)
 
         });
       } else {
@@ -73,8 +116,8 @@ export class GeneratorComponent implements OnInit {
     }
   }
 
-  
-  buttonMessage = (msg: string) =>{
+
+  buttonMessage = (msg: string) => {
     this.msg.changeMessage(msg);
   }
 
